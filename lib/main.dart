0f82ac6d09f614002480aefccc335ed9d26d9512
bijuice <abdul.r.zaid@gmail.com>
2020-12-19 12:01:27 +0300

    Complete

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,38 +11,5 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(title: 'Welcome to Flutter', home: RandomWords());
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _randomWordPairs = <WordPair>[];
-
-  Widget _buildList() {
-    return (ListView(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, item) {
-        if (item.isOdd) return Divider();
-
-        final index = item ~/ 2;
-
-        if (index >= _randomWordPairs.length) {
-          _randomWordPairs.addAll(generateWordPairs().take(10));
-        }
-      },
-    ));
-  }
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('WordPair Generator'),
-      ),
-      body: _buildList(),
-    );
   }
 }
